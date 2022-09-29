2608fbc5c96deb1d35a201893716175b2e712df3
helen tan <helentanmz0909@gmail.com>
2022-09-29 17:53:58 +0800

    Login attempt 1

--- app/components/HeaderLoggedOut.js
+++ app/components/HeaderLoggedOut.js
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { Axios } from "axios";
 
 const HeaderLoggedOut = () => {
+  const [username, setUsername] = useState()
+  const [password, setPassword] = useState()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      const response = await Axios.post('http://localhost:8080/login', {username, password})
+      console.log(response.data)
+    } catch(err) {
+      console.log('There was a problem')
+    }
+  }
+
   return (
-    <form className="mb-0 pt-2 pt-md-0">
+    <form onSubmit={handleSubmit} className="mb-0 pt-2 pt-md-0">
       <div className="row align-items-center">
         <div className="col-md mr-0 pr-md-0 mb-3 mb-md-0">
-          <input name="username" className="form-control form-control-sm input-dark" type="text" placeholder="Username" autoComplete="off" />
+          <input onChange={e => setUsername(e.target.value)} name="username" className="form-control form-control-sm input-dark" type="text" placeholder="Username" autoComplete="off" />
         </div>
         <div className="col-md mr-0 pr-md-0 mb-3 mb-md-0">
-          <input name="password" className="form-control form-control-sm input-dark" type="password" placeholder="Password" />
+          <input onChange={e => setPassword(e.target.value)} name="password" className="form-control form-control-sm input-dark" type="password" placeholder="Password" />
         </div>
         <div className="col-md-auto">
           <button className="btn btn-success btn-sm">Sign In</button>
